2d9b7b256814514aef5100d0a60710a2b17767f5
Gratic <blairodu74@yahoo.fr>
2024-09-13 11:29:32 +0200

    ci: Automatic tests and linting on pull request for master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
+      <Image
+        src="/minecraft.jpg"
+        width={225}
+        height={225}
+        alt="Minecraft image"
+      />
     </div>
   );
 }
