96ee82d071e24240ec2ffb5d2830780e107aabe0
Gratic <blairodu74@yahoo.fr>
2024-09-15 19:43:49 +0200

    feat: logic of ListOfTaskCards

--- src/lib/ui/ListOfTaskCards.tsx
+++ src/lib/ui/ListOfTaskCards.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import TaskCard from "@/lib/ui/TaskCard";
+import { useSearchParams } from "next/navigation";
+import { readCheckedStateFromParam } from "@/lib/utils";
 
 export default function ListOfTaskCards() {
     const tasks = [
@@ -7,9 +11,21 @@ export default function ListOfTaskCards() {
         { id: "id3", title: "title of 3", content: "content of 3", completedAt: '2024-09-15'},
     ]
 
+    const params = useSearchParams();
+
+    const showCompleted = readCheckedStateFromParam(params, "showCompleted", true);
+    const showTodo = readCheckedStateFromParam(params, "showTodo", true);
+    const orderBy = params.get("order") ?? "chronologically";
+
+    const showedTasks = tasks
+        .filter(task => (showCompleted && showTodo) || (showCompleted && task.completedAt) || (showTodo && !task.completedAt));
+    
+    if (orderBy === "reverse")
+        showedTasks.reverse();
+
     return (
         <div className="bg-white bg-opacity-40 backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden transition-all duration-300 hover:shadow-2xl">
-            {tasks.map((task, index, arr) => 
+            {showedTasks.map((task, index, arr) => 
                 <TaskCard task={task} index={index} last={arr.length-1 === index} />
             )}
         </div>
--- src/lib/ui/datatypes.ts
+++
@@ -1,6 +0,0 @@
-export type Task = {
-    id: string,
-    title: string,
-    content: string,
-    completedAt: string | null
-};
