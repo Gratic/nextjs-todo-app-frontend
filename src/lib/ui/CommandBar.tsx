14fb00414ac6b2e4ba5403162e8301d3132d3134
Gratic <blairodu74@yahoo.fr>
2024-09-15 19:19:27 +0200

    feat: logic of CommandBar component

--- src/lib/ui/CommandBar.tsx
+++ src/lib/ui/CommandBar.tsx
@@ -1,4 +1,52 @@
+'use client';
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function CommandBar() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+    
+    const [selectValue, setSelectValue] = useState(searchParams.get("order") ?? "chronological");
+
+    function readCheckedStateFromParam(key: string, if_null_return: boolean = true) {
+        const val = searchParams.get(key);
+        if (typeof val === "string") return val === "true";
+        return if_null_return;
+    }
+
+    const [showCompleted, setShowCompleted] = useState(readCheckedStateFromParam("showCompleted"));
+    const [showTodo, setShowTodo] = useState(readCheckedStateFromParam("showTodo"));
+
+    function handleOrderChange(value: string) {
+        const params = new URLSearchParams(searchParams)
+
+        setSelectValue(value);
+        params.set("order", value);
+
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    function handleShowCompletedChange() {
+        const params = new URLSearchParams(searchParams)
+
+        setShowCompleted(!showCompleted);
+        params.set("showCompleted", String(!showCompleted));
+
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    function handleShowTodoChange() {
+        const params = new URLSearchParams(searchParams)
+
+        setShowTodo(!showTodo);
+        params.set("showTodo", String(!showTodo));
+
+        replace(`${pathname}?${params.toString()}`);
+    }
+
     return (
         <div className="bg-white bg-opacity-40 backdrop-blur-xl rounded-2xl shadow-xl
             p-6 mb-8
@@ -8,9 +56,15 @@ export default function CommandBar() {
                 <label htmlFor="order" className="mr-3 text-gray-700 font-medium">
                     Order by:
                 </label>
-                <select name="order" id="order" className="form-select bg-white bg-opacity-50 border border-gray-300 text-gray-700 py-2 px-4 rounded-lg focus-ring-indigo-500 transition-all duration-300">
-                    <option value="">Chronologically</option>
-                    <option value="">Reverse</option>
+                <select 
+                    name="order" 
+                    id="order" 
+                    className="form-select bg-white bg-opacity-50 border border-gray-300 text-gray-700 py-2 px-4 rounded-lg focus-ring-indigo-500 transition-all duration-300"
+                    onChange={e => handleOrderChange(e.currentTarget.value)}
+                    value={selectValue}
+                >
+                    <option value="chronologically">Chronologically</option>
+                    <option value="reverse">Reverse</option>
                 </select>
             </div>
 
@@ -18,9 +72,10 @@ export default function CommandBar() {
                 <div className="flex items-center">
                     <input 
                         type="checkbox"
-                        name="showCompleted"
                         id="completed"
                         className="form-checkbox h-5 w-5 text-indigo-600 rounded transition duration-150 ease-in-out"
+                        checked={showCompleted}
+                        onChange={() => handleShowCompletedChange()}
                     />
                     <label htmlFor="completed" className="ml-2 text-gray-700">
                         Completed
@@ -29,9 +84,10 @@ export default function CommandBar() {
 
                 <div className="flex items-center">
                     <input type="checkbox"
-                        name="showTodo"
                         id="todo"
                         className="form-checkbox h-5 w-5 text-indigo-600 rounded transition duration-150 ease-in-out"
+                        checked={showTodo}
+                        onChange={() => handleShowTodoChange()}
                     />
                     <label htmlFor="todo" className="ml-2 text-gray-700">
                         Todo
