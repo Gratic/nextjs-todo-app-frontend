e03c530209aa9a6ab609479ab1203332dfb2d654
Gratic <blairodu74@yahoo.fr>
2024-09-17 00:27:03 +0200

    feat: add Add button and isAdding state, add callback function to createNewTask

--- src/lib/ui/ListOfTaskCards.tsx
+++ src/lib/ui/ListOfTaskCards.tsx
@@ -4,11 +4,13 @@ import TaskCard from "@/lib/ui/TaskCard";
 import { useSearchParams } from "next/navigation";
 import { readCheckedStateFromParam } from "@/lib/utils";
 import { getAllTasks } from "../task_rest_api";
+import { createTask } from "../actions";
+import { useState } from "react";
+import NewTaskForm from "./NewTaskForm";
+import { Task } from "../datatypes";
 
 
 export default function ListOfTaskCards() {
-    const { tasks, isLoading, isError } = getAllTasks();
-
     // const tasks = [
     //     { id: "id1", title: "title of 1", content: "content of 1", completedAt: null},
     //     { id: "id2", title: "title of 2", content: "content of 2", completedAt: null},
@@ -16,10 +18,12 @@ export default function ListOfTaskCards() {
     // ]
 
     const params = useSearchParams();
-
+    const { tasks, isLoading, isError, mutate } = getAllTasks();
     const showCompleted = readCheckedStateFromParam(params, "showCompleted", true);
     const showTodo = readCheckedStateFromParam(params, "showTodo", true);
     const orderBy = params.get("order") ?? "chronologically";
+
+    const [isAdding, setIsAdding] = useState(false);
 
     if (isLoading) return <p>Loading...</p>;
     if (isError) return <p>An error occured.</p>;
@@ -29,8 +33,29 @@ export default function ListOfTaskCards() {
     if (orderBy === "reverse")
         showedTasks.reverse();
 
+    async function createNewTask(newTask: Omit<Task, "id">) {
+        const createdTask = await createTask(newTask);
+        tasks?.push(createdTask) ?? [createdTask];
+        mutate(tasks);
+
+        setIsAdding(false);
+    }
+
     return (
         <div className="bg-white bg-opacity-40 backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden transition-all duration-300 hover:shadow-2xl">
+            { isAdding ? (
+                <NewTaskForm onSubmit={createNewTask} onCancel={() => setIsAdding(false)} />
+            ) : (
+                <button 
+                    onClick={() => setIsAdding(true)}
+                    className="w-full p-4 text-indigo-600 hover:bg-indigo-50 transition-colors duration-300 flex items-center justify-center"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+                    </svg>
+                    <span className="ml-2">Add Task</span>
+                </button>
+            )}
             {showedTasks.map((task, index, arr) => 
                 <TaskCard key={task.id} task={task} index={index} last={arr.length-1 === index} />
             )}
