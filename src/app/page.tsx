a66cfc61fec71f1409e2caacd2145709f2af7e8c
Gratic <blairodu74@yahoo.fr>
2024-09-19 22:27:34 +0200

    feat: modify metadata, add a favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-'use client';
-
+import type { Metadata } from 'next'
 import CommandBar from "@/lib/ui/CommandBar";
 import ListOfTaskCards from "@/lib/ui/ListOfTaskCards";
-import { BiPlus } from "react-icons/bi";
+ 
+export const metadata: Metadata = {
+  title: 'Simple Todo App',
+  description: 'Web application for managing a simple todo list.',
+}
 
 export default function Home() {
 
     return (
         <main className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-100 p-8 font-sans">
             <div className="max-w-3xl mx-auto">
-
                 {/* Header */}
                 <div className="text-center mb-12 transform hover:scale-105 transition-transform duration-300">
                     <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-600 mb-3">Simple Task App</h1>
